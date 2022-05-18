681dfb0e98097037c753836a432d92af62cf77fc
Kaustubh Agrawal <kaustubh@alphabi.co>
2022-05-18 18:18:36 +0530

    basic opted list

--- screens/OptedScreen.js
+++ screens/OptedScreen.js
@@ -1,18 +1,78 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View, Text} from 'react-native';
+import {Box, ScrollView, Text, Flex} from 'native-base';
+import React, {useState, useEffect} from 'react';
+import constants from '../constants';
+
+const ListFromOptions = ({title, list, color}) => {
+  return (
+    <Box m={4}>
+      <Text mt="3" fontWeight="extrabold" fontSize="xl">
+        {title}
+      </Text>
+      <Box>
+        <Flex gap={2}>
+          {list.map((univ, i) => (
+            <Box
+              p={2}
+              bgColor={color + '.300'}
+              borderRadius="md"
+              w="100%"
+              background="gray.200"
+              key={i}>
+              <Text fontSize="lg" fontWeight="bold">
+                {univ.name}
+              </Text>
+              <Text fontSize="md">{univ.countyName}</Text>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
 // create a component
-export default function OptedScreen() {
+export default function OptedScreen({id, authKey}) {
+  const [opted, setOpted] = useState({target: [], reach: [], safety: []});
+
+  useEffect(() => {
+    fetch(`${constants.BACKEND_URL}/student/getOptions`, {
+      headers: {
+        Authorization: `Bearer ${authKey}`,
+      },
+    })
+      .then(res => res.json())
+      .then(response => {
+        const {data} = response;
+        const opt = data.reduce(
+          (acc, d) => {
+            const {university, type} = d;
+            acc[type].push(university);
+            return acc;
+          },
+          {target: [], reach: [], safety: []},
+        );
+        setOpted(opt);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-      }}>
-      <Text>Opted!</Text>
-    </View>
+    <Flex flex={1} background={'white'}>
+      <ScrollView>
+        <ListFromOptions
+          color="red"
+          title="Target"
+          list={opted.target}></ListFromOptions>
+        <ListFromOptions
+          color="blue"
+          title="Reach"
+          list={opted.reach}></ListFromOptions>
+        <ListFromOptions
+          color="green"
+          title="Safety"
+          list={opted.safety}></ListFromOptions>
+      </ScrollView>
+    </Flex>
   );
 }
