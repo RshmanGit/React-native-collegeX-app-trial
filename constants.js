a92f5961464ade4345688a15eeeef1d06c71eab8
Kaustubh Agrawal <kaustubh@alphabi.co>
2022-05-19 14:30:00 +0530

    profile screen updates

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 export default {
-  BACKEND_URL: 'https://7444-2405-201-200a-3163-2982-ea1c-fab0-90c7.in.ngrok.io'
-}
+  BACKEND_URL: 'https://cefb-2405-201-200a-3163-fc15-7277-192-c3c1.in.ngrok.io',
+};
